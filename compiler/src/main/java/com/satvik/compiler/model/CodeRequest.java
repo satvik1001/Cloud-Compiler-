0e01366c1fa28d97f761c34dc07b27a800c41ba8
satvik1001 <164597742+satvik1001@users.noreply.github.com>
2026-05-28 23:45:58 +0530

    Create CodeRequest class for code submission model

---
+++ compiler/src/main/java/com/satvik/compiler/model/CodeRequest.java
@@ -0,0 +1,40 @@
+package com.satvik.compiler.model;
+
+public class CodeRequest {
+
+    private String code;
+
+    private String language;
+
+    private String input;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(
+            String code
+    ) {
+        this.code = code;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(
+            String language
+    ) {
+        this.language = language;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(
+            String input
+    ) {
+        this.input = input;
+    }
+}
